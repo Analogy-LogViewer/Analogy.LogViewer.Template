6d591eb81fb0f68b2fbfff5233bf91fe3356b286
Lior Banai <liorbanai@gmail.com>
2019-12-08 19:59:34 +0200

    initial commit

---
+++ Analogy.LogViewer.Template/Analogy.LogViewer.Template/ChangeLogList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Analogy.Interfaces;
+
+namespace Analogy.LogViewer.Template
+{
+    public static class ChangeLogList
+    {
+        public static IEnumerable<AnalogyChangeLog> GetChangeLog()
+        {
+            yield return new AnalogyChangeLog("Example (template)", AnalogChangeLogType.None, "Lior Banai", new DateTime(2019, 12, 08));
+        }
+    }
+}
