2b2942c5cade7914f4fa262ca8e7b2bba8049b27
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-06 20:43:08 +0300

    add CustomActionsFactory implementation

---
+++ Analogy.LogViewer.Template/IAnalogy/CustomActionsFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analogy.Interfaces;
+using Analogy.Interfaces.Factories;
+
+namespace Analogy.LogViewer.Template
+{
+    public abstract class CustomActionsFactory: IAnalogyCustomActionsFactory
+    {
+        public abstract Guid FactoryId { get; set; }
+        public virtual string Title { get; set; } = "Custom Action";
+        public abstract IEnumerable<IAnalogyCustomAction> Actions { get; }
+    }
+}
