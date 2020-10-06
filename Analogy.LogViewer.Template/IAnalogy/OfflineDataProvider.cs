dacc74065e7d4970ec338ace1a783009d17a6142
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-06 20:34:29 +0300

    ..

--- Analogy.LogViewer.Template/IAnalogy/OfflineDataProvider.cs
+++ Analogy.LogViewer.Template/IAnalogy/OfflineDataProvider.cs
@@ -1,4 +1,6 @@
 ﻿using Analogy.Interfaces;
+using Analogy.LogViewer.Template.Managers;
+using Analogy.LogViewer.Template.Properties;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -6,8 +8,6 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Analogy.LogViewer.Template.Managers;
-using Analogy.LogViewer.Template.Properties;
 
 namespace Analogy.LogViewer.Template
 {
@@ -22,12 +22,12 @@ namespace Analogy.LogViewer.Template
         public virtual string FileOpenDialogFilters { get; set; } = string.Empty;
         public virtual string FileSaveDialogFilters { get; set; } = string.Empty;
         public virtual IEnumerable<string> SupportFormats { get; set; } = Array.Empty<string>();
-        public virtual string InitialFolderFullPath { get; set; } = string.Empty;
+        public virtual string InitialFolderFullPath { get; } = string.Empty;
         public virtual bool UseCustomColors { get; set; } = false;
         public virtual IEnumerable<(string originalHeader, string replacementHeader)> GetReplacementHeaders()
             => Array.Empty<(string, string)>();
 
-        public virtual  (Color backgroundColor, Color foregroundColor) GetColorForMessage(IAnalogyLogMessage logMessage)
+        public virtual (Color backgroundColor, Color foregroundColor) GetColorForMessage(IAnalogyLogMessage logMessage)
             => (Color.Empty, Color.Empty);
 
         public abstract Task<IEnumerable<AnalogyLogMessage>> Process(string fileName, CancellationToken token,
@@ -36,14 +36,14 @@ namespace Analogy.LogViewer.Template
 
         public IEnumerable<FileInfo> GetSupportedFiles(DirectoryInfo dirInfo, bool recursiveLoad) =>
             GetSupportedFilesInternal(dirInfo, recursiveLoad);
-       
-        public virtual Task SaveAsync(List<AnalogyLogMessage> messages, string fileName) =>Task.CompletedTask;
+
+        public virtual Task SaveAsync(List<AnalogyLogMessage> messages, string fileName) => Task.CompletedTask;
 
         public virtual bool CanOpenFile(string fileName) => SupportFormats.Any(pattern =>
                 CommonUtilities.Files.FilesPatternMatcher.StrictMatchPattern(pattern, fileName));
-        
+
         public virtual bool CanOpenAllFiles(IEnumerable<string> fileNames) => fileNames.All(CanOpenFile);
-        
+
         public virtual Task InitializeDataProviderAsync(IAnalogyLogger logger)
         {
             LogManager.Instance.SetLogger(logger);
@@ -52,7 +52,7 @@ namespace Analogy.LogViewer.Template
 
         public virtual void MessageOpened(AnalogyLogMessage message)
         {
-           //noop
+            //noop
         }
         protected virtual List<FileInfo> GetSupportedFilesInternal(DirectoryInfo dirInfo, bool recursive)
         {
