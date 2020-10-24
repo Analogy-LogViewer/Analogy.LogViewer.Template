41701ddafa5704ab24fa862c9b6fb3ee5d701493
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-24 12:27:39 +0300

    add notifications and update mechanism

---
+++ Analogy.LogViewer.Template/IAnalogy/AnalogyDownloadInformation.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Analogy.Interfaces.DataTypes;
+
+namespace Analogy.LogViewer.Template.IAnalogy
+{   
+    /// <summary>
+     ///Object of this class gives you all the details about the update useful in handling the update logic yourself.
+     /// </summary>
+    [Serializable]
+    public abstract class AnalogyDownloadInformation : IAnalogyDownloadInformation
+    {
+        /// <summary>
+        /// ID of the primary Factory it belongs to
+        /// </summary>
+        public Guid PrimaryFactoryId { get; set; }
+        /// <summary>
+        /// The component title/name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// If new update is available then returns true otherwise false.
+        /// </summary>
+        public bool IsUpdateAvailable { get; set; }
+
+        /// <summary>
+        /// Download URL of the update file.
+        /// </summary>
+        public string? DownloadURL { get; set; }
+
+        /// <summary>
+        /// URL of the webpage specifying changes in the new update.
+        /// </summary>
+        public string? ChangeLogURL { get; set; }
+
+        public string? LatestVersionNumber { get; set; }
+        /// <summary>
+        /// Returns newest version of the application available to download.
+        /// </summary>
+        public Version? LatestVersion
+        {
+            get
+            {
+                if (LatestVersionNumber == null)
+                    return null;
+                try
+                {
+                    return new Version(LatestVersionNumber);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+
+        private string? _installedVersionNumber;
+        public virtual string InstalledVersionNumber
+        {
+            get
+            {
+                if (_installedVersionNumber != null)
+                {
+                    return _installedVersionNumber;
+                }
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+                _installedVersionNumber = fvi.FileVersion;
+                return _installedVersionNumber;
+            }
+
+        }
+
+        /// <summary>
+        ///     Returns version of the application currently installed on the user's PC.
+        /// </summary>
+        public virtual Version InstalledVersion
+        {
+            get
+            {
+                try
+                {
+                    return new Version(InstalledVersionNumber);
+                }
+                catch (Exception)
+                {
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+                    _installedVersionNumber = fvi.FileVersion;
+                    return new Version(fvi.FileVersion);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Shows if the update is required or optional.
+        /// </summary>
+        public bool Mandatory { get; set; }
+
+        /// <summary>
+        ///Command line arguments used by Installer.
+        /// </summary>
+        public string? InstallerArgs { get; set; }
+
+        /// <summary>
+        ///Checksum of the update file.
+        /// </summary>
+        public string? Checksum { get; set; }
+
+        /// <summary>
+        ///Hash algorithm that generated the checksum.
+        /// </summary>
+        public string? HashingAlgorithm { get; set; }
+
+        protected AnalogyDownloadInformation(Guid primaryFactoryId, string name)
+        {
+            Name = name;
+            PrimaryFactoryId = primaryFactoryId;
+        }
+
+        protected AnalogyDownloadInformation(Guid primaryFactoryId, string name, bool isUpdateAvailable, string? downloadUrl, string? changeLogUrl, string? latestVersionNumber, string? installedVersionNumber, bool mandatory, string? installerArgs, string? checksum, string? hashingAlgorithm) : this(primaryFactoryId, name)
+        {
+            IsUpdateAvailable = isUpdateAvailable;
+            DownloadURL = downloadUrl;
+            ChangeLogURL = changeLogUrl;
+            LatestVersionNumber = latestVersionNumber;
+            _installedVersionNumber = installedVersionNumber;
+            Mandatory = mandatory;
+            InstallerArgs = installerArgs;
+            Checksum = checksum;
+            HashingAlgorithm = hashingAlgorithm;
+        }
+    }
+}
--- Analogy.LogViewer.Template/IAnalogy/PrimaryFactory.cs
+++ Analogy.LogViewer.Template/IAnalogy/PrimaryFactory.cs
@@ -9,13 +9,16 @@ namespace Analogy.LogViewer.Template
 {
     public abstract class PrimaryFactory : IAnalogyFactory
     {
+        public event EventHandler<IAnalogyNotification>? OnRaiseNotification;
         public abstract Guid FactoryId { get; set; }
         public virtual string Title { get; set; } = "Data Provider";//override this
         public virtual IEnumerable<IAnalogyChangeLog> ChangeLog { get; set; } = ChangeLogList.GetChangeLog();
         public virtual IEnumerable<string> Contributors { get; set; } = new List<string> { "Lior Banai" };
         public virtual string About { get; set; } = "Log Parser for Analogy Log Viewer";//override this
+
         public virtual Image? SmallImage { get; set; } = Resources.Analogy16x16;
         public virtual Image? LargeImage { get; set; } = Resources.Analogy32x32;
-
+        
+        protected void RaiseNotification(object sender, IAnalogyNotification notification) => OnRaiseNotification?.Invoke(sender, notification);
     }
 }
