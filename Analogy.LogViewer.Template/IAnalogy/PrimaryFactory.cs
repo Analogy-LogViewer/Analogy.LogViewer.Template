6be543237af8a781e9d5622c9ad1856a1386c390
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-24 13:13:26 +0300

    update notification and download mechanism

--- Analogy.LogViewer.Template/IAnalogy/PrimaryFactory.cs
+++ Analogy.LogViewer.Template/IAnalogy/PrimaryFactory.cs
@@ -1,15 +1,15 @@
 ﻿using Analogy.Interfaces;
 using Analogy.Interfaces.Factories;
+using Analogy.LogViewer.Template.Managers;
+using Analogy.LogViewer.Template.Properties;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using Analogy.LogViewer.Template.Properties;
 
 namespace Analogy.LogViewer.Template
 {
     public abstract class PrimaryFactory : IAnalogyFactory
     {
-        public event EventHandler<IAnalogyNotification>? OnRaiseNotification;
         public abstract Guid FactoryId { get; set; }
         public virtual string Title { get; set; } = "Data Provider";//override this
         public virtual IEnumerable<IAnalogyChangeLog> ChangeLog { get; set; } = ChangeLogList.GetChangeLog();
@@ -18,7 +18,9 @@ namespace Analogy.LogViewer.Template
 
         public virtual Image? SmallImage { get; set; } = Resources.Analogy16x16;
         public virtual Image? LargeImage { get; set; } = Resources.Analogy32x32;
-        
-        protected void RaiseNotification(object sender, IAnalogyNotification notification) => OnRaiseNotification?.Invoke(sender, notification);
+
+        public void RegisterNotificationCallback(INotificationReporter notificationReporter)
+        => NotificationManager.Instance.SetReporter(notificationReporter);
+
     }
 }
---
+++ Analogy.LogViewer.Template/Managers/NotificationManager.cs
@@ -0,0 +1,20 @@
+﻿using Analogy.Interfaces;
+using System;
+
+namespace Analogy.LogViewer.Template.Managers
+{
+    public class NotificationManager : INotificationReporter
+    {
+        private static readonly Lazy<NotificationManager> _instance = new Lazy<NotificationManager>();
+        public static NotificationManager Instance => _instance.Value;
+        private INotificationReporter? Reporter { get; set; }
+
+        public void SetReporter(INotificationReporter notificationReporter) => Reporter = notificationReporter;
+
+
+        public void RaiseNotification(IAnalogyNotification notification, bool showAsPopup)
+        {
+            Reporter?.RaiseNotification(notification, showAsPopup);
+        }
+    }
+}
